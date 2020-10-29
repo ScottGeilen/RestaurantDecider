f97e683bba0e2459191df03afaba1618b472df37
Scott <scottgeilen99@gmail.com>
2020-10-29 16:30:29 -0400

    v1.2 - Added DoorDash, fast food, and dine in options

--- FoodDecider.py
+++ FoodDecider.py
@@ -12,8 +12,8 @@ root.geometry('500x150')
 myLabel = Label(root, text="Tucson, AZ Restaurant Decider ")
 myLabel.pack()
     
-def suggestFood():
-    doorDash = ["Popeyes"
+def suggestDoorDash():
+    doorDash = ["Popeyes",
                   "It's Just Wings",
                   "Raising Cane's",
                   "McDonald's",
@@ -98,9 +98,133 @@ def suggestFood():
                   "Scordato's Pizzeria",
                   "Frog & Firkin",
                   "Golden Corral",]
+    fastFood = [
+                  "Five Guys",
+                  "Raising Cane's",
+                  "KFC",
+                  "Subway",
+                  "Smashburger",
+                  "Jack in the Box",
+                  "Carl's Jr.",
+                  "Arby's",
+                  "Five Guys",
+                  "Wendy's",
+                  "Burger King",
+                  "Taco Bell",
+                ]
+    dineIn = ['']
+    takeOut = ['']
     choice = random.choice(doorDash)
+    choice = random.choice(fastFood)
     myLabel = Label(root, text='You should consider ordering '+str(choice)+'!')
     myLabel.pack()
-btn = Button(root, text='Where should I eat?', bd='5', command=suggestFood)
-btn.pack()
+def suggestFastFood():
+    fastFood = [
+                "Five Guys",
+                "Raising Cane's",
+                "KFC",
+                "Subway",
+                "Smashburger",
+                "Jack in the Box",
+                "Carl's Jr.",
+                "Arby's",
+                "Five Guys",
+                "Wendy's",
+                "Burger King",
+                "Taco Bell",
+                "Chipotle",
+                "Firehouse Subs",
+                "Dairy Queen",
+                "Popeyes",
+                "McDonald's",
+                ]
+    choice = random.choice(fastFood)
+    myLabel = Label(root, text='You should consider ordering '+str(choice)+'!')
+    myLabel.pack()
+def suggestDineIn():
+    dineIn = [
+                "Popeyes",
+                "It's Just Wings",
+                "McDonald's",
+                "Riliberto's Fresh Mexican Food",
+                "MOD Pizza",
+                "Baskin Robins",
+                "Wienerschnitzel",
+                "El Beto Mexican Food",
+                "El Pollo Loco",
+                "Jersey Mike's Subs",
+                "Jimmy John's Sandwiches",
+                "Panera Bread",
+                "Lucky Wishbone",
+                "Panda Express",
+                "Blake's Lotaburger",
+                "Freddy's Frozen Custard & Steakburgers",
+                "Firehouse Subs",
+                "Dairy Queen",
+                "Sarku Japan",
+                "Charleys Philly Steaks",
+                "No Anchovies",
+                "IHOP",
+                "Trident Pizza Pub",
+                "Eegee's",
+                "Jason's Deli",
+                "Hooters",
+                "Applebee's Grill & Bar",
+                "Viva Burrito",
+                "Cheba Hut",
+                "The Cheesecake Factory",
+                "Beyond Bread",
+                "Famous Dave's",
+                "The Oink Cafe",
+                "Claim Jumper",
+                "Red Robin Gourmet Burgers and Brews",
+                "Los Betos",
+                "Denny's",
+                "Black Bear Diner",
+                "Chili's Grill & Bar",
+                "Buffalo Wild Wings",
+                "The Habit Burger",
+                "Craft Republic",
+                "Salty Dawg II",
+                "The Buffalo Spot",
+                "Chipotle",
+                "Wingstop",
+                "Sauce Pizza & Wine",
+                "Pita Jungle",
+                "Guilin Chinese Restaurant",
+                "Truland Burgers & Greens",
+                "Schlotzsky's Austin Eatery",
+                "Brooklyn Pizza Company",
+                "Empire Pizza",
+                "North Italia",
+                "Oregano's",
+                "Piezanos Rustic Pizza and Wine",
+                "Postino",
+                "Carrabba's",
+                "Renee's Organic Oven",
+                "Guiseppe's",
+                "Piazza Gavi",
+                "Romano's Lunch Express",
+                "Green Things",
+                "Kenney D's",
+                "Humble Pie",
+                "Southwest Grillers",
+                "Native Grill & Wings",
+                "BJ's Restaurant * Brewhouse",
+                "Pizza Patron",
+                "Magpies Gourmet Pizza",
+                "Fired Pie",
+                "Scordato's Pizzeria",
+                "Frog & Firkin",
+                "Golden Corral",
+                ]
+    choice = random.choice(dineIn)
+    myLabel = Label(root, text='You should consider ordering '+str(choice)+'!')
+    myLabel.pack()
+DD = Button(root, text='Feeling DoorDash?', bd='5', command=suggestDoorDash)
+FF = Button(root, text='Feeling fast food?', bd='5', command=suggestFastFood)
+DI = Button(root, text='Feeling dine in?', bd='5', command=suggestDineIn)
+DD.pack()
+FF.pack()
+DI.pack()
 root.mainloop()
